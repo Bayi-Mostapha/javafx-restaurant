872852e47ef1a9924c01a4b47537471186883c22
Mostapha Bayi <133959392+Bayi-Mostapha@users.noreply.github.com>
2024-03-19 09:22:32 +0000

    Update module-info.java

--- src/application/module-info.java
+++ src/application/module-info.java
@@ -1,1 +1,12 @@
-
+module TableViewSQL2023 {
+	requires javafx.controls;
+	requires java.sql;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.base;
+	requires javafx.swing;
+	
+	opens application.Controllers to javafx.fxml;
+	opens application to javafx.graphics, javafx.fxml, javafx.base;
+	exports application.Controllers;
+}
